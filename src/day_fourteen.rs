027e8f723f621dee8bdacca5d631893f4286447b
Michael Edwards <medwards@walledcity.ca>
2021-12-14 19:49:25 +0100

    Clean up Day 14 solver and replace Part One /w Part Two impl

--- src/day_fourteen.rs
+++ src/day_fourteen.rs
@@ -1,6 +1,4 @@
 use std::cmp::max;
-use std::collections::HashMap;
-use std::iter::once;
 
 use anyhow::Result;
 
@@ -8,37 +6,8 @@ pub const INPUT_PATH: &str = "inputs/day/14/input";
 
 pub fn part_one(contents: &str) -> Result<usize> {
     let (template, rules) = load_polymerization(contents)?;
-    let polymer = (0..10).fold(template, |template, _| {
-        apply_rules(template.as_slice(), rules.as_slice())
-    });
 
-    let counts = polymer
-        .iter()
-        .fold(HashMap::<_, usize>::new(), |mut counts, element| {
-            *counts.entry(element).or_default() += 1;
-            counts
-        });
-
-    Ok(counts
-        .values()
-        .max()
-        .ok_or_else(|| anyhow::anyhow!("empty polymer"))?
-        - counts
-            .values()
-            .min()
-            .ok_or_else(|| anyhow::anyhow!("empty_polymer"))?)
-}
-
-pub fn part_two(contents: &str) -> Result<usize> {
-    let (template, rules) = load_polymerization(contents)?;
-
-    let pairs = template_to_pair_counts(template.as_slice(), rules.as_slice());
-
-    let pairs = (0..40).fold(pairs, |pairs, _| {
-        apply_rules_count(&pairs, rules.as_slice())
-    });
-
-    let counts = count_from_pairs(&pairs, template.as_slice());
+    let counts = process_polymer(template.as_slice(), rules.as_slice(), 10);
 
     Ok(counts
         .iter()
@@ -51,23 +20,33 @@ pub fn part_two(contents: &str) -> Result<usize> {
             .ok_or_else(|| anyhow::anyhow!("empty_polymer"))?)
 }
 
-fn apply_rules(template: &[u8], rules: &[(u8, u8, u8)]) -> Vec<u8> {
-    template
-        .windows(2)
-        .flat_map(|window| {
-            let rule = rules
-                .iter()
-                .find(|rule| window[0] == rule.0 && window[1] == rule.1)
-                .expect("No matching rule");
-            vec![rule.0, rule.2]
-        })
-        .chain(once(*template.last().expect("empty template")))
-        .collect()
+pub fn part_two(contents: &str) -> Result<usize> {
+    let (template, rules) = load_polymerization(contents)?;
+
+    let counts = process_polymer(template.as_slice(), rules.as_slice(), 40);
+
+    Ok(counts
+        .iter()
+        .max()
+        .ok_or_else(|| anyhow::anyhow!("empty polymer"))?
+        - counts
+            .iter()
+            .filter(|count| **count != 0)
+            .min()
+            .ok_or_else(|| anyhow::anyhow!("empty_polymer"))?)
 }
 
-fn apply_rules_count(pair_counts: &Vec<Vec<usize>>, rules: &[(u8, u8, u8)]) -> Vec<Vec<usize>> {
-    let mut result = vec![vec![0; pair_counts.len()]; pair_counts.len()];
-    pair_counts.iter().enumerate().for_each(|(i, row)| {
+fn process_polymer(template: &[u8], rules: &[(u8, u8, u8)], iterations: usize) -> Vec<usize> {
+    let pairs = template_to_pairs(template, rules);
+
+    let pairs = (0..iterations).fold(pairs, |pairs, _| apply_rules(&pairs, rules));
+
+    count_from_pairs(&pairs, template)
+}
+
+fn apply_rules(pairs: &Vec<Vec<usize>>, rules: &[(u8, u8, u8)]) -> Vec<Vec<usize>> {
+    let mut result = vec![vec![0; pairs.len()]; pairs.len()];
+    pairs.iter().enumerate().for_each(|(i, row)| {
         row.iter().enumerate().for_each(|(j, count)| {
             if let Some((_, _, output)) = rules
                 .iter()
@@ -99,7 +78,7 @@ fn count_from_pairs(pairs: &Vec<Vec<usize>>, template: &[u8]) -> Vec<usize> {
     counts
 }
 
-fn template_to_pair_counts(template: &[u8], rules: &[(u8, u8, u8)]) -> Vec<Vec<usize>> {
+fn template_to_pairs(template: &[u8], rules: &[(u8, u8, u8)]) -> Vec<Vec<usize>> {
     let max = (*max(template.iter().max(), rules.iter().map(|(_, _, o)| o).max()).unwrap_or(&0)
         - b'A') as usize;
     let mut counts = vec![vec![0; max + 1]; max + 1];
@@ -157,18 +136,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_apply_rules() {
-        let (template, rules) =
-            load_polymerization(read_to_string("fixtures/polymer.txt").unwrap().as_str()).unwrap();
-
-        assert_eq!(
-            "NCNBCHB".as_bytes(),
-            apply_rules(template.as_slice(), rules.as_slice())
-        );
-    }
-
-    #[test]
-    fn test_template_to_counts() {
+    fn test_template_to_pairs() {
         let (template, rules) =
             load_polymerization(read_to_string("fixtures/polymer.txt").unwrap().as_str()).unwrap();
 
@@ -179,46 +147,45 @@ mod tests {
 
         assert_eq!(
             expected,
-            template_to_pair_counts(template.as_slice(), rules.as_slice())
+            template_to_pairs(template.as_slice(), rules.as_slice())
         );
     }
 
     #[test]
-    fn test_apply_rules_count() {
+    fn test_apply_rules() {
         let (template, rules) =
             load_polymerization(read_to_string("fixtures/polymer.txt").unwrap().as_str()).unwrap();
 
-        let expected = template_to_pair_counts("NCNBCHB".as_bytes(), rules.as_slice());
-        let mut old_counts = template_to_pair_counts(template.as_slice(), rules.as_slice());
-        let counts = apply_rules_count(&old_counts, rules.as_slice());
+        let expected = template_to_pairs("NCNBCHB".as_bytes(), rules.as_slice());
+        let mut old_counts = template_to_pairs(template.as_slice(), rules.as_slice());
+        let counts = apply_rules(&old_counts, rules.as_slice());
         assert_eq!(expected, counts);
 
-        let expected = template_to_pair_counts("NBCCNBBBCBHCB".as_bytes(), rules.as_slice());
+        let expected = template_to_pairs("NBCCNBBBCBHCB".as_bytes(), rules.as_slice());
         old_counts = counts;
-        let counts = apply_rules_count(&old_counts, rules.as_slice());
+        let counts = apply_rules(&old_counts, rules.as_slice());
         assert_eq!(expected, counts);
 
-        let expected =
-            template_to_pair_counts("NBBBCNCCNBBNBNBBCHBHHBCHB".as_bytes(), rules.as_slice());
+        let expected = template_to_pairs("NBBBCNCCNBBNBNBBCHBHHBCHB".as_bytes(), rules.as_slice());
         old_counts = counts;
-        let counts = apply_rules_count(&old_counts, rules.as_slice());
+        let counts = apply_rules(&old_counts, rules.as_slice());
         assert_eq!(expected, counts);
 
-        let expected = template_to_pair_counts(
+        let expected = template_to_pairs(
             "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB".as_bytes(),
             rules.as_slice(),
         );
         old_counts = counts;
-        let counts = apply_rules_count(&old_counts, rules.as_slice());
+        let counts = apply_rules(&old_counts, rules.as_slice());
         assert_eq!(expected, counts);
     }
 
     #[test]
-    fn test_pair_counts_to_counts() {
+    fn test_counts_from_pairs() {
         let (_, rules) =
             load_polymerization(read_to_string("fixtures/polymer.txt").unwrap().as_str()).unwrap();
 
-        let pairs = template_to_pair_counts(
+        let pairs = template_to_pairs(
             "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB".as_bytes(),
             rules.as_slice(),
         );
